86902b5c85ce437ff993c9ca04f89be591c66e52
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-26 15:49:15 -0400

    feat: add txt_importter function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if '.txt' not in path_file:
+        return sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
