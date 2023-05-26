93c1379055929486e6389a4e0dd86be07fc1a45d
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-26 16:09:25 -0400

    test: add test for priority queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    queue.enqueue({
+        "nome_do_arquivo": "teste1.txt",
+        "qtd_linhas": 6,
+    })
+    queue.enqueue({
+        "nome_do_arquivo": "teste2.txt",
+        "qtd_linhas": 3,
+    })
+    assert len(queue) == 2
+    assert queue.search(1)["nome_do_arquivo"] == "teste1.txt"
+    queue.dequeue()
+    assert len(queue) == 1
+    with pytest.raises(IndexError):
+        queue.search(4)
+    assert queue.is_priority({
+        "nome_do_arquivo": "teste1.txt",
+        "qtd_linhas": 6,
+    }) is False
