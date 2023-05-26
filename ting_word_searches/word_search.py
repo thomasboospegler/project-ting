c91ef18d93e34fd8b1d99d419d117da8a945c0e6
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-26 16:21:29 -0400

    feat: add sech word functions

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,38 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words = []
+    search_word = word.lower()
+
+    for element in instance._data:
+        for i in range(len(element["linhas_do_arquivo"])):
+            if search_word in element["linhas_do_arquivo"][i].lower():
+                words.append({"linha": i + 1})
+        if len(words) > 0:
+            return [{
+                "palavra": word,
+                "arquivo": element["nome_do_arquivo"],
+                "ocorrencias": words
+                }]
+        return []
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    search_word = word.lower()
+
+    for element in instance._data:
+        data = {
+            "palavra": word,
+            "arquivo": element["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        count = 1
+        for line in element["linhas_do_arquivo"]:
+            if search_word in line.lower():
+                data["ocorrencias"].append(
+                    {"linha": count, "conteudo": line}
+                )
+            count += 1
+        if len(data["ocorrencias"]) != 0:
+            result.append(data)
+        data["ocorrencias"] == []
+    return result
