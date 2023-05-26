ea8181208f1a0a692f3ff1b611bdb62a0b1a2219
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-26 15:57:32 -0400

    feat: add file_process functions

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+    if len(instance) > 0:
+        for file in instance._data:
+            if file['nome_do_arquivo'] == path_file:
+                return None
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+    file_name = instance.search(0)["nome_do_arquivo"]
+    instance.dequeue()
+    return sys.stdout.write(
+        f"Arquivo {file_name} removido com sucesso\n"
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return sys.stdout.write(str(instance.search(position)))
+    except IndexError:
+        return sys.stderr.write(
+            "Posição inválida"
+        )
